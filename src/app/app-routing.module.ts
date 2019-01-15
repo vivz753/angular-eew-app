f984f6f28483535ba39204cae8c0c6940cd0c9a3
Vivian Leung <vivz753@gmail.com>
2019-01-14 19:49:39 -0800

    link components to respective routes (home, registration, about)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RegistrationComponent } from './registration/registration.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'register',
+    component: RegistrationComponent,
+  }
+  // ,
+  // {
+  //   path: 'about',
+  //   component: AboutComponent,
+  // }
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
